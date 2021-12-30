541a85bc11322b4d947835644d1540fa0e1c1b9d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-12-30 09:11:07 +0530

    Added: checking emp present or absent by using random method

--- src/main/kotlin/com/empwage/kotlin/EmployeeWage.kt
+++ src/main/kotlin/com/empwage/kotlin/EmployeeWage.kt
@@ -1,5 +1,16 @@
 package com.empwage.kotlin
 
 fun main(args: Array<String>) {
-    print("Welcome to the Employee wage program")
+    println("Welcome to the Employee wage program")
+
+    val isAbsent = 0
+    val isPresent = 1
+    if (randNumber(isAbsent, isPresent) == 0) {
+       println("Employee is Absent")
+    } else {
+        println("Employee is Present")
+    }
+}
+fun randNumber(isAbsent: Int, isPresent: Int): Int {
+    return (isAbsent..isPresent).random()
 }
