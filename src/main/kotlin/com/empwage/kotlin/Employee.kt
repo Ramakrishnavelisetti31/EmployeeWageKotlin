6a10f28874c74f9aa641b7813d39abbaaff0ef3c
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2022-01-03 20:06:09 +0530

    Added: Calculate employee part time wage

--- src/main/kotlin/com/empwage/kotlin/Employee.kt
+++ src/main/kotlin/com/empwage/kotlin/Employee.kt
@@ -3,25 +3,29 @@ package com.empwage.kotlin
 fun main(args: Array<String>) {
     println("Welcome to the Employee wage program")
     val employee = Employee()
-    val isAbsent = 0
-    val isPresent = 1
+    val isFullTime = 1
+    val isPartTime = 2
     val wagePerHour = 20
-    println(employee.checkDailyWage(isAbsent, isPresent, wagePerHour))
+    println(employee.checkDailyWage(isFullTime, isPartTime, wagePerHour))
 }
 
 class Employee {
-    private fun randNumber(isAbsent: Int, isPresent: Int): Int {
-        return (isAbsent..isPresent).random()
+    private fun randNumber( isFullTime: Int, isPartTime: Int): Int {
+        return (isFullTime..isPartTime).random()
     }
 
-    fun checkDailyWage(isAbsent: Int, isPresent: Int, wagePerHour: Int) {
-        if (randNumber(isAbsent, isPresent) == 0) {
-            val dailyHours = 0
-            println("Employee is Absent")
+    fun checkDailyWage( isFullTime: Int, isPartTime: Int, wagePerHour: Int) {
+        if (randNumber(isFullTime, isPartTime) == 1) {
+            val dailyHours = 8
+            println("Employee is Present for Full Time")
+            println("The Employee Wage is ${calculateWage(wagePerHour, dailyHours)}")
+        } else if(randNumber(isFullTime, isPartTime) == 2){
+            val dailyHours = 4
+            println("Employee is Present for Part Time")
             println("The Employee Wage is ${calculateWage(wagePerHour, dailyHours)}")
         } else {
-            val dailyHours = 8
-            println("Employee is Present")
+            val dailyHours = 0
+            println("Employee is Absent")
             println("The Employee Wage is ${calculateWage(wagePerHour, dailyHours)}")
         }
     }
